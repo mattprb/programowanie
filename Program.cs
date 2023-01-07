00e29def8697eb36b3445d88c319012fa8cc0442
Mateusz <106449642+mattprb@users.noreply.github.com>
2023-01-07 11:02:13 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,62 +1,57 @@
-﻿using Microsoft.VisualBasic;
-using System.ComponentModel.DataAnnotations;
-using System.Drawing;
-using System.IO.Enumeration;
-using System.Net.Http.Headers;
-using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Threading.Channels;
-using static System.Net.Mime.MediaTypeNames;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace projekt1
 {
-
     internal class Program
     {
         static void Main(string[] args)
         {
-
             bool loop = true;
             do
             {
-                Console.WriteLine("");
-                Console.WriteLine("Podaj nazwę miasta:");
-                string miasto = Console.ReadLine();
-                Console.WriteLine("Podaj imie, nazwisko i PESEL");
-                string imie, nazwisko, pesel;
-                string[] input = Console.ReadLine().Split();
-                imie = input[0];
-                nazwisko = input[1];
-                pesel = input[2];
-
-                //poprawność PESELU
-                if (PeselCheck.IsValid(pesel))
+                try
                 {
-                    string fileName = pesel + ".txt";
-                    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                    string filePath = Path.Combine(desktopPath, fileName);
-                    //jeżeli jest plik na pulpicie o nazwie *pesel* to wtedy go nadpisz
-                    if (File.Exists(filePath))
+                    Console.WriteLine("");
+                    Console.WriteLine("Podaj nazwę miasta:");
+                    string miasto = Console.ReadLine();
+                    Console.WriteLine("Podaj imie, nazwisko i PESEL (oddzielone spacjami)");
+                    string imie, nazwisko, pesel;
+                    string[] input = Console.ReadLine().Split();
+                    imie = input[0];
+                    nazwisko = input[1];
+                    pesel = input[2];
+
+                    //poprawność PESELU
+                    if (PeselCheck.IsValid(pesel))
                     {
-                        Console.WriteLine("Plik o numerze PESEL: {0} już istnieje! Został on nadpisany.", pesel);
-                        File.WriteAllText(fileName, "");
-                        loop = false;
-                    }           
+                        string fileName = pesel + ".txt";
+                        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                        string filePath = Path.Combine(desktopPath, fileName);
+                        //jeżeli jest plik na pulpicie o nazwie *pesel* to wtedy go nadpisz
+                        if (File.Exists(filePath))
+                        {
+                            Console.WriteLine("Plik z numerem PESEL: {0} już istnieje! Został on nadpisany nowymi danymi.", pesel);
+                            File.WriteAllText(fileName, "");
+                        }
                         StreamWriter writer = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite));
                         writer.WriteLine("{0}", miasto);
                         writer.WriteLine("{0} {1} {2}", imie, nazwisko, pesel);
                         writer.WriteLine("");
-                        writer.Close();                
+                        writer.Close();
+                        loop = false; //pętla kończy się po wpisaniu do pliku danych
+                    }
+                    else Console.WriteLine("Niepoprawny PESEL!");
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine("Niepoprawny PESEL!");                 
+                    Console.WriteLine("Błąd. Szczegóły: {0} ", e);
                 }
             }
             while (loop); 
-
-
         }
-
     }
 }
