a203befdb90f82286b1ca32ec09720a3433e58d9
Mateusz <106449642+mattprb@users.noreply.github.com>
2023-01-08 21:28:44 +0100

    Add files via upload

---
+++ PeselCheckTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using projekt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class PeselCheckTests
+    {
+        [Test]
+        public void IsValid_CorrectPesel_ReturnsTrue()
+        {
+            // Arrange
+            string correctPesel = "75121968629";
+
+            // Act
+            bool result = PeselCheck.IsValid(correctPesel);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void IsValid_IncorrectPeselLength_ReturnsFalse()
+        {
+            // Arrange
+            string incorrectPesel = "1234567890";
+
+            // Act
+            bool result = PeselCheck.IsValid(incorrectPesel);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void IsValid_IncorrectCheckSum_ReturnsFalse()
+        {
+            // Arrange
+            string incorrectPesel = "01234567890";
+
+            // Act
+            bool result = PeselCheck.IsValid(incorrectPesel);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class ProgramTests
+    {
+        [Test]
+        public void TestCreateDirectory()
+        {
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string folderName = Path.Combine(desktopPath, "dane");
+
+
+            if (Directory.Exists(folderName))
+            {
+                Directory.Delete(folderName, true);
+            }
+
+            
+            Directory.CreateDirectory(folderName);
+
+            
+            Assert.IsTrue(Directory.Exists(folderName));
+        }
+    }
+}
